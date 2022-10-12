282a86b03807ba34ad4d916af972d87c2f2745ea
Luiz Gustavo Retka Darossi <luizgustavodarossi@gmail.com>
2022-10-12 11:28:07 -0300

    Base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://todo-list-n3cb-allji7t4d-luizgustavodarossi.vercel.app/'
+  baseURL: 'http://localhost::3000/'
 })
